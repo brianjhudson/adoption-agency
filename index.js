38c920237e413ea2f1a8e34b7c0c28c2ddba9c78
brianjhudson <brianjasonhudson@gmail.com>
2024-03-03 19:25:55 -0600

    Add get endpoints for all entities

--- index.js
+++ index.js
@@ -3,6 +3,34 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res, next) => {
+    return res.json({message: 'Welcome to the Pet Adoption Agency.'});
+});
+
+app.get('/animal-types', (req, res, next) => {
+    return res.json([]);
+})
+
+app.get('/animals', (req, res, next) => {
+    return res.json([]);
+})
+
+app.get('/adoptable-animals', (req, res, next) => {
+    return res.json([]);
+})
+
+app.get('/adopted-animals', (req, res, next) => {
+    return res.json([]);
+})
+/*
+// Sample endpoints to demonstrate REST express defaults:
+    app.get('/animal', () => {});
+    app.post('/animal', () => {});
+    // app.patch('/animal', () => {});
+    app.put('/animal', () => {});
+    app.delete('/animal', () => {});
+*/
+
 
 app.listen(3000, () => {
     console.log('Server is now listening.');
